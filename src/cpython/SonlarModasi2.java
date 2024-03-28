96eaf9d8cdde96a23633adc71d2e35cb16f1ebe3
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-28 07:12:31 +0500

    moda2

---
+++ src/cpython/SonlarModasi2.java
@@ -0,0 +1,33 @@
+package cpython;
+
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SonlarModasi2 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int[] numbers = new int[n];
+    for (int i = 0; i < n; i++) {
+      numbers[i] = scanner.nextInt();
+    }
+
+    Map<Integer, Integer> frequencyMap = new HashMap<>();
+    for (int number : numbers) {
+      frequencyMap.put(number, frequencyMap.getOrDefault(number, 0) + 1);
+    }
+
+    int maxFrequency = Collections.max(frequencyMap.values());
+    int moda = Integer.MAX_VALUE;
+    for (Map.Entry<Integer, Integer> entry : frequencyMap.entrySet()) {
+      if (entry.getValue() == maxFrequency && entry.getKey() < moda) {
+        moda = entry.getKey();
+      }
+    }
+
+    System.out.println(moda);
+  }
+}
