147fbba1d91092cfca0f0247c120a6a276c96d8f
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-28 07:10:16 +0500

    list

---
+++ src/cpython/MargeList.java
@@ -0,0 +1,33 @@
+package cpython;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MargeList {
+
+  public static void main(String[] args) {
+
+    ArrayList<Integer> list1 = new ArrayList<Integer>();
+    ArrayList<Integer> list2 = new ArrayList<Integer>();
+
+    Random random = new Random();
+
+    for (int i = 0; i < 100; i++) {
+      list1.add(random.nextInt(1000) + 1);
+      list2.add(random.nextInt(1000) + 1);
+    }
+
+    System.out.println("List 1: " + list1);
+    System.out.println("List 2: " + list2);
+
+    ArrayList<Integer> mergedList = new ArrayList<Integer>();
+    mergedList.addAll(list1);
+    mergedList.addAll(list2);
+
+    Collections.sort(mergedList);
+
+    System.out.println("Birlashgan listlar" + mergedList);
+  }
+
+}
