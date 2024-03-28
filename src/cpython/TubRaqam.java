b804be6cd7add4464b73e999a72051f2b61581ba
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-28 07:10:35 +0500

    isPrime ?

---
+++ src/cpython/TubRaqam.java
@@ -0,0 +1,25 @@
+package cpython;
+
+public class TubRaqam {
+
+  public static void main(String[] args) {
+    //Berilgan natural sonni istalgan bir tub bo'lgan raqamini toping.Yagona qatorda masala yechimini chiqaring. Agar yechim mavjud bo'lmasa -1 ni chiqaring.
+
+    int n = 893;
+    int tub = 0;
+    for (int i = 2; i < n; i++) {
+      if (n % i != 0) {
+        tub = -1;
+        break;
+      }
+    }
+    if (tub != 0) {
+      System.out.println(2);
+    } else {
+      System.out.println(tub);
+    }
+
+
+  }
+
+}
---
+++ src/cpython/TubSonlar.java
@@ -0,0 +1,30 @@
+package cpython;
+
+import java.util.Scanner;
+
+//Sizga n soni berilgan. [1;n] oralig'ida bo'lgan sonlar orasida barcha tub sonlarni ekranga chiqaring.
+public class TubSonlar {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("n sonini kiriting: ");
+    int n = scanner.nextInt();
+    System.out.println("Tub sonlar:");
+    for (int i = 2; i <= n; i++) {
+      if (isPrime(i)) {
+        System.out.print(i + " ");
+      }
+    }
+  }
+  public static boolean isPrime(int number) {
+    if (number <= 1) {
+      return false;
+    }
+    for (int i = 2; i <= Math.sqrt(number); i++) {
+      if (number % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
