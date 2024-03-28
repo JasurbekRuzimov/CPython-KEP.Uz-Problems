580de37610174b4246864993c201aaa7a877f3e7
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-03-28 07:13:43 +0500

    KEP 85

---
+++ src/cpython/KEP_85.java
@@ -0,0 +1,11 @@
+package cpython;
+
+public class KEP_85 {
+  //73+81-32^84%101*115/116
+
+  public static void main(String[] args) {
+    int result = 73+81-32^84%101*115/116;
+    System.out.println(result);
+  }
+
+}
