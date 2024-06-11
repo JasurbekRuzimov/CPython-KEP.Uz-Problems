b472f0de65ca35ceaeff3e69a8ae61fcc2f177a5
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-11 17:02:20 +0500

    Raqamlar yig'indisi

--- src/cpython/asoslar/RaqqamlarYigindisi.java
+++ src/cpython/asoslar/RaqqamlarYigindisi.java
@@ -1,2 +1,17 @@
-package cpython.asoslar;public class RaqqamlarYigindisi {
+package cpython.asoslar;
+import java.util.Scanner;
+public class RaqqamlarYigindisi {
+  //Berilgan sonni raqamlar yig'indisini hisoblovchi dastur tuzing.
+
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+      int num = scanner.nextInt();
+      int sum = 0;
+      while (num != 0) {
+        sum += num % 10;
+        num /= 10;
+      }
+      System.out.println(sum);
+    }
+
 }
