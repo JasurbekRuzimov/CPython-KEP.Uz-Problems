5ff7bc459c0dfab6b91d15f59fa390427ad8df8e
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-11 17:02:54 +0500

    To'rt xonali sonlar

--- src/cpython/asoslar/TortXonaliSonlar.java
+++ src/cpython/asoslar/TortXonaliSonlar.java
@@ -1,2 +1,22 @@
-package cpython.asoslar;public class TortXonaliSonlar {
+package cpython.asoslar;
+
+public class TortXonaliSonlar {
+// Barcha 4 xonali sonlar ichida raqamlari yig'indisi juft bo'lgan
+// sonlarni ekranga chop etuvchi dastur tuzing.
+  public static void main(String[] args) {
+    for (int i = 1000; i <= 9999; i++) {
+      if (yigindiJuftmi(i)) {
+        System.out.print(i+" ");
+      }
+    }
+  }
+
+  private static boolean yigindiJuftmi(int num) {
+    int sum = 0;
+    while (num != 0) {
+      sum += num % 10;
+      num /= 10;
+    }
+    return sum % 2 == 0;
+  }
 }
