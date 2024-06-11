38d24632897862c365625de6296059b9619e37fd
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-11 17:01:45 +0500

    Caps Lock

--- src/cpython/asoslar/cAPSLOCK.java
+++ src/cpython/asoslar/cAPSLOCK.java
@@ -1,2 +1,26 @@
-package cpython.asoslar;public class cAPSLOCK {
+package cpython.asoslar;
+
+import java.util.Scanner;
+
+public class cAPSLOCK {
+
+  //Satr berilgan bo'lib, satr caps lock orqali yozilganligini tekshiring.
+// Satr caps lock orqali yozilgan, agarda satrni birinchi harfi kichik,
+// qolganlari esa katta bo'lsa yoki barcha harflari katta harflar bo'lsa.
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    String input = scanner.nextLine();
+    if (input.isEmpty()) {
+      System.out.println("False");
+    } else if (input.equals(input.toUpperCase())) {
+      System.out.println("True");
+    } else if (input.length() > 1 && input.substring(1).equals(input.substring(1).toUpperCase())) {
+      System.out.println("True");
+    } else {
+      System.out.println("False");
+    }
+  }
 }
+
