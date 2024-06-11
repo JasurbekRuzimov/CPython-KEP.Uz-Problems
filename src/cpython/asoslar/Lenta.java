f954a74951740f22fa426d8fe625a27d6f03d4c0
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-11 17:01:52 +0500

    Lenta

--- src/cpython/asoslar/Lenta.java
+++ src/cpython/asoslar/Lenta.java
@@ -1,2 +1,20 @@
-package cpython.asoslar;public class Lenta {
+package cpython.asoslar;
+
+import java.util.Scanner;
+
+public class Lenta {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n, m;
+    n = scanner.nextInt();
+    m = scanner.nextInt();
+    int total = n / m;
+    if (n % m != 0) {
+      total++;
+    }
+    System.out.println(total);
+
+  }
+
 }
