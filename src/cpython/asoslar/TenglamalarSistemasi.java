c344bdd7a696e4dc51f28c09aade92fa3318732a
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-11 17:02:39 +0500

    Tenglamalar sistemasi

--- src/cpython/asoslar/TenglamalarSistemasi.java
+++ src/cpython/asoslar/TenglamalarSistemasi.java
@@ -1,2 +1,27 @@
-package cpython.asoslar;public class TenglamalarSistemeasi {
+package cpython.asoslar;
+
+import java.util.Scanner;
+
+public class TenglamalarSistemasi {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int a = scanner.nextInt();
+    int b = scanner.nextInt();
+
+    double discriminant = a * a - 4 * b;
+
+    double x1 = (a + Math.sqrt(discriminant)) / 2;
+    double x2 = (a - Math.sqrt(discriminant)) / 2;
+
+    double y1 = a - x1;
+    double y2 = a - x2;
+
+    if (x1 > 0 && y1 > 0) {
+      System.out.println(x1 + " " + y1);
+    } else if (x2 > 0 && y2 > 0) {
+      System.out.println(x2 + " " + y2);
+    } else {
+      System.out.println("");
+    }
+  }
 }
