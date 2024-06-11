1791d6f5a14c24412e6a454ead40cea34abb1aec
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-11 17:01:23 +0500

    Bo'linmaydigan son

--- src/cpython/asoslar/BolinmaydiganSon.java
+++ src/cpython/asoslar/BolinmaydiganSon.java
@@ -1,2 +1,11 @@
-package cpython.asoslar;public class BolinmaydiganSon {
+package cpython.asoslar;
+
+public class BolinmaydiganSon {
+//Sizga n soni berilgan. Sizni vazifangiz
+// n sonini bo'luvchisi bo'lmagan eng kichik natural sonni topish. scanner bilan ishlash kerak
+
+
+
+  
+
 }
