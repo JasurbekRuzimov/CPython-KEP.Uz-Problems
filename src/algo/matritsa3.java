9336ca35d8e642035e7ec743569ac89decdc03bb
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2024-06-28 17:55:41 +0500

    matritsa 3 from algo

--- src/algo/matritsa3.java
+++ src/algo/matritsa3.java
@@ -1,2 +1,37 @@
-package algo;public class matritsa3 {
-}
+package algo;
+
+import java.util.Scanner;
+
+public class matritsa3 {
+    public static void main(String[] args) {
+      Scanner scanner = new Scanner(System.in);
+
+      int L = scanner.nextInt();
+      int[] elements = new int[L];
+      for (int i = 0; i < L; i++) {
+        elements[i] = scanner.nextInt();
+      }
+      int n = scanner.nextInt();
+      int m = scanner.nextInt();
+
+      int[][] matrix = new int[n][m];
+      int index = 0;
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          if (index < L) {
+            matrix[i][j] = elements[index++];
+          } else {
+            matrix[i][j] = 0;
+          }
+        }
+      }
+
+      for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+          System.out.print(matrix[i][j] + " ");
+        }
+        System.out.println();
+      }
+    }
+  }
+
