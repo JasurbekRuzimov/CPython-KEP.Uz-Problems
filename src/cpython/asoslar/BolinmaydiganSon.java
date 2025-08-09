3a5286e82cb981531cb96bcebf85f0451a1e1c41
Jasurbek Ruzimov <ruzimovjasur2105@gmail.com>
2025-08-09 12:02:45 +0500

    24. Bo'linmaydigan son ✅

--- src/cpython/asoslar/BolinmaydiganSon.java
+++ src/cpython/asoslar/BolinmaydiganSon.java
@@ -1,11 +1,24 @@
 package cpython.asoslar;
 
+import java.util.Scanner;
+
 public class BolinmaydiganSon {
 //Sizga n soni berilgan. Sizni vazifangiz
 // n sonini bo'luvchisi bo'lmagan eng kichik natural sonni topish. scanner bilan ishlash kerak
 
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt();
+
+    for (int i=1; i<=n; i++){
+      if (n%i != 0){
+        System.out.println(i);
+        break;
+      }
+    }
 
 
-  
+  }
 
 }
